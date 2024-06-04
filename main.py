cf7bdd5584daf2746ecfb042c660e604615b2b94
kushagra1303 <120771776+kushagra1303@users.noreply.github.com>
2024-06-04 23:12:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, jsonify, request
+from flask_pymongo import PyMongo
+
+from openai import OpenAI
+import os
+# Assuming you've set your API key securely (see previous explanation)
+api_key = os.getenv("OPENAI_API_KEY","")  # Retrieve from environment variable
+client = OpenAI(api_key=api_key)  # Create client object
+
+
+
+
+
+
+# from openai import OpenAI
+# client = OpenAI("sk-proj-5UgVekBfqZBFLGrRqqdJT3BlbkFJrkbtSbB3oZYE3jt4bWKG")
+
+
+
+
+# openai.api_key = "sk-proj-5UgVekBfqZBFLGrRqqdJT3BlbkFJrkbtSbB3oZYE3jt4bWKG"
+
+
+
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/chatgpt"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+    chats = mongo.db.chats.find({})
+    myChats = [chat for chat in chats]
+    print(myChats)
+    return render_template("index.html", myChats = myChats)
+
+@app.route("/api", methods=["GET", "POST"])
+def qa():
+    if request.method == "POST":
+        print(request.json)
+        question = request.json.get("question")
+        chat = mongo.db.chats.find_one({"question": question})
+        print(chat)
+        if chat:
+            data = {"question": question, "answer": f"{chat['answer']}"}
+            return jsonify(data)
+        else:
+            response = client.chat.completions.create(
+                        model="gpt-3.5-turbo-16k",
+                        messages=[question],
+                        temperature=1,
+                        max_tokens=256,
+                        top_p=1,
+                        frequency_penalty=0,
+                        presence_penalty=0
+                    )
+            print(response)
+            data = {"question": question, "answer": response["choices"][0]["text"]}
+            mongo.db.chats.insert_one({"question": question, "answer": response["choices"][0]["text"]})
+            return jsonify(data)
+    data = {"result": "Thank you! I'm just a machine learning model designed to respond to questions and generate text based on my training data. Is there anything specific you'd like to ask or discuss? "}
+        
+    return jsonify(data)
+
+app.run(debug=True, port=5001)
---
+++ readme.md
@@ -0,0 +1,4 @@
+This is a my CHATBOT (DEVELOPER @KUSHAGRA BHARGAVA)
+Which uses OpenAI (API) to fetch data from the servers and load it on our webpage.
+Tech stack used -> HTML , FLASK , MONGODB , Tailwind CSS , OpenAi API
+Hope you like it :)
